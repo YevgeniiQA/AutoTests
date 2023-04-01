47933dac30eb7e776c0742a8b7e5a372a194e2f9
Tiger <tigetsd.tm@gmail.com>
2023-04-01 15:55:05 +0300

    Merge remote-tracking branch 'origin/master'

--- test_todomvc_e2e.py
+++ test_todomvc_e2e.py
@@ -1,17 +1,17 @@
 from time import sleep
-from selene import have
+from selene import have, command
 from selene.support.shared import browser
 from selene.support.shared.jquery_style import s, ss
 
-browser.open('http://todomvc.com/examples/emberjs')
+
 
 def test_e2e():
+    browser.open('http://todomvc.com/examples/emberjs')
     s('#new-todo').type('11').press_enter()
     s('#new-todo').type('22').press_enter()
     s('#new-todo').type('33').press_enter()
     s('#new-todo').type('44').press_enter()
     ss('#todo-list li').should(have.exact_texts('11', '22', '33', '44'))
-    sleep(1)
     ss('#todo-list li').element_by(have.exact_text('22')).element('.toggle').click()
     ss('#todo-list li').element_by(have.exact_text('33')).element('.toggle').click()
     ss('#todo-list li').element_by(have.exact_text('22')).element('.toggle').click()
@@ -22,7 +22,10 @@ def test_e2e():
     ss('#todo-list li').element_by(have.exact_text('33')).hover().element('.destroy').click()
     ss('#todo-list li').should(have.size(0))
     s('[href="#/"]').click()
-    ss('#todo-list li').element_by(have.exact_text('22')).double_click().type('edited').press_enter()
-    ss('#todo-list li').element_by(have.css_class('.editing'))#.element('.edit')
-    # s('#new-todo').type('11').press_enter()
-    sleep(2)
+    ss('#todo-list li').element_by(have.exact_text('22')).double_click()
+    ss('#todo-list li').element_by(have.css_class('editing')).element('.edit').perform(command.js.set_value('11223344')).press_enter()
+    s('#toggle-all').click()
+    ss('#todo-list li').element_by(have.exact_text('11')).element('.toggle').click()
+    s('#clear-completed').click()
+    ss('#todo-list li').should(have.size(1))
+    sleep(3)
